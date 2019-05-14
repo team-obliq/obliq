6fc17a6558a1c5975fd2da7cc944588dcde84090
Carlo Munguia <carlomunguia@carlos-air.home>
2019-05-14 15:09:13 -0500

    first commit

---
+++ src/main/java/com/obliq/obliq/ObliqApplication.java
@@ -0,0 +1,13 @@
+package com.obliq.obliq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObliqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObliqApplication.class, args);
+    }
+
+}
