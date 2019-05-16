bee776f4ccea88ae9c57f52c9973e5da6b8b5415
Travis Vela <jtravisvela@gmail.com>
2019-05-16 16:48:04 -0500

    add individual post view. add Get mapping to the post controller for this view.

--- src/main/java/com/obliq/obliq/CTRL/home_CTRL.java
+++ src/main/java/com/obliq/obliq/CTRL/home_CTRL.java
@@ -1,14 +1,30 @@
 package com.obliq.obliq.CTRL;
 
+import com.obliq.obliq.REPOS.CommentRepository;
+import com.obliq.obliq.REPOS.PostRespository;
+import com.obliq.obliq.REPOS.UserRespository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class home_CTRL {
 
-    @GetMapping("/index")
-    public String indexPage() {
+    private PostRespository postRepo;
+    private UserRespository userRepo;
+    private CommentRepository commentRepo;
 
+    public home_CTRL(PostRespository postRepo, UserRespository userRepo, CommentRepository commentRepo) {
+        this.postRepo = postRepo;
+        this.userRepo = userRepo;
+        this.commentRepo = commentRepo;
+    }
+
+    @GetMapping("/")
+    public String indexPage(Model model) {
+        model.addAttribute("posts", postRepo.findAll());
+        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("comments", commentRepo.findAll());
         return "index";
     }
 }
--- src/main/java/com/obliq/obliq/CTRL/post_CTRL.java
+++ src/main/java/com/obliq/obliq/CTRL/post_CTRL.java
@@ -1,6 +1,7 @@
 package com.obliq.obliq.CTRL;
 
 import com.obliq.obliq.ENTITYS.Post;
+import com.obliq.obliq.REPOS.CommentRepository;
 import com.obliq.obliq.REPOS.PostRespository;
 import com.obliq.obliq.REPOS.UserRespository;
 import org.springframework.stereotype.Controller;
@@ -16,10 +17,20 @@ public class post_CTRL {
 //    repo injection
     private PostRespository postRepo;
     private UserRespository userRepo;
+    private CommentRepository commentRepo;
 
-    public post_CTRL(PostRespository postRepo, UserRespository userRepo) {
+    public post_CTRL(PostRespository postRepo, UserRespository userRepo, CommentRepository commentRepo) {
         this.postRepo = postRepo;
         this.userRepo = userRepo;
+        this.commentRepo = commentRepo;
+    }
+
+//    map to show individual posts
+    @GetMapping("/posts/showPost/{id}")
+    public String showPost(@PathVariable long id, Model model) {
+        model.addAttribute("post", postRepo.findOne(id));
+        model.addAttribute("comments", commentRepo.findByPostId(id));
+        return "posts/showPost";
     }
 
 //    map for edit form in the edit view
