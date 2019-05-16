4e346a5dbb57da8b8c28ff9300e6bda7187750a4
Travis Vela <jtravisvela@gmail.com>
2019-05-16 13:40:10 -0500

    add delete function to the post repo. tested

--- src/main/java/com/obliq/obliq/CTRL/post_CTRL.java
+++ src/main/java/com/obliq/obliq/CTRL/post_CTRL.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class post_CTRL {
 
+//    repo injection
     private PostRespository postRepo;
     private UserRespository userRepo;
 
@@ -21,6 +22,7 @@ public class post_CTRL {
         this.userRepo = userRepo;
     }
 
+//    map for edit form in the edit view
     @GetMapping("/posts/edit/{id}")
     public String showEditForm(@PathVariable long id, Model model) {
         Post post = postRepo.findOne(id);
@@ -28,10 +30,19 @@ public class post_CTRL {
         return "posts/edit";
     }
 
+//    map for edited post. save to DB
     @PostMapping("posts/edit/{id}")
     public String editPost(@ModelAttribute Post postEdited) {
         postEdited.setUser(userRepo.findOne(1L));
         postRepo.save(postEdited);
+        return "redirect:/profile";
+    }
+
+//    map for delete function
+    @GetMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable long id) {
+        Post post = postRepo.findOne(id);
+        postRepo.delete(post);
         return "redirect:/profile";
     }
 
