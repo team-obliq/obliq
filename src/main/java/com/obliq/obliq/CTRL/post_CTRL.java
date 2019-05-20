155d0ab854c4a1a3dcc85c8dcaeaa1a52747b04d
Travis Vela <jtravisvela@gmail.com>
2019-05-20 15:04:07 -0500

    add create post mapping and view. add navbar links to all views.

--- src/main/java/com/obliq/obliq/CTRL/post_CTRL.java
+++ src/main/java/com/obliq/obliq/CTRL/post_CTRL.java
@@ -35,6 +35,21 @@ public class post_CTRL {
         return "posts/showPost";
     }
 
+//    map for creating posts
+    @GetMapping("/posts/create")
+    public String showCreatePostForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@ModelAttribute Post post) {
+        User sessionUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        post.setUser(userRepo.findOne(sessionUser.getId()));
+        postRepo.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+
 //    map for edit form in the edit view
     @GetMapping("/posts/edit/{id}")
     public String showEditForm(@PathVariable long id, Model model) {
